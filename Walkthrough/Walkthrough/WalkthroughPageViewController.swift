3627e07581b8e070545c6d21ef8b248d0e2f4d59
KATHYSO <sokathy46@gmail.com>
2018-12-09 16:47:18 +0800

    code update

--- Walkthrough/Walkthrough/WalkthroughPageViewController.swift
+++ Walkthrough/Walkthrough/WalkthroughPageViewController.swift
@@ -9,22 +9,59 @@
 import UIKit
 
 class WalkthroughPageViewController: UIViewController {
+    
+    weak var pageViewController: UIPageViewController!
+    
+    let walkthroughInfo: WalkthroughInfo
+    
+    init(walkthroughInfo: WalkthroughInfo) {
+        self.walkthroughInfo = walkthroughInfo
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.addPageViewController()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func addPageViewController() {
+        // Add Page View Controller
+        let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: [:])
+        pageViewController.view.backgroundColor = .black
+        pageViewController.dataSource = self
+        pageViewController.delegate = self
+        
+        guard let pageView = pageViewController.view else { return }
+        
+        self.addChild(pageViewController)
+        pageView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(pageView)
+        
+//        pageView.
+        
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[pageView]|", options: [], metrics: nil, views: ["pageView": pageView]))
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[pageView]|", options: [], metrics: nil, views: ["pageView": pageView]))
+        
+        pageViewController.didMove(toParent: self)
+        self.pageViewController = pageViewController
     }
-    */
+}
 
+extension WalkthroughPageViewController: UIPageViewControllerDelegate {
+    
+}
+
+extension WalkthroughPageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        <#code#>
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        <#code#>
+    }
 }
